75df18f9e7b1566680ad7465431ec696def6c78a
mstoews <mstoews@hotmail.com>
2022-12-12 13:01:20 +0900

    Update header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { CommonModule, Location } from '@angular/common';
 import { IconsModule } from 'app/icons.module';
 import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -12,15 +13,25 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class HeaderComponent {
 
-  constructor (private _location: Location) {
+  constructor (
+    private _location: Location,
+    private _router: Router
+    ) {
     this.title = "Add Title as Parameter in the Template"
   }
 
   @Input() title : string;
   @Input() sub_title : string;
+  @Input() back: boolean;
+  @Input() home: boolean;
 
   public onBack(){
     this._location.back()
   }
+
+  public onHome(){
+    this._router.navigate(['home']);
+  }
+
 
 }
