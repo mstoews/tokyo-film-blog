644460baba618e1efe45d7d29fa4eb0c82de3325
murray <mstoews@hotmail.com>
2023-01-22 09:41:44 -0500

    update cart

---
+++ src/app/features/ui/products/product-simple/product-simple.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-simple',
+  templateUrl: './product-simple.component.html',
+  styleUrls: ['./product-simple.component.css']
+})
+export class ProductSimpleComponent {
+
+}
